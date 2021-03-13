a14d17e7d2f3a8f8f7ad27bf30bf93d3594a8b1f
tracer-nju <tracer@nju.edu.cn>
2021-03-13 10:36:04 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: d25dd1313e4f4e03a021e3bd5cee4d5b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  10:36:04 up 13:53,  1 user,  load average: 0.46, 0.67, 0.69

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,32 +1,25 @@
 #include <assert.h>
 #include <getopt.h>
 #include <stdio.h>
-#include <stdbool.h>
 
-
-struct option long_options[] = {
-  {"show-pids",    no_argument, 0, 'p'},
-  {"numeric-sort", no_argument, 0, 'n'},
-  {"version",      no_argument, 0, 'V'},
-  {0,              0,           0,  0 }
-};
+int flag_p = 0, flag_n = 0, flag_V = 0;
 
 int main(int argc, char* argv[]) {
-  bool flag_p = false, flag_n = false, flag_V = false;
+  static struct option long_options[] = {{"show-pids", no_argument, 0, 'p'},
+                                         {"numeric-sort", no_argument, 0, 'n'},
+                                         {"version", no_argument, 0, 'V'},
+                                         {0, 0, 0, 0}};
+
   int opt;
-  while ((opt = getopt_long(argc, argv, "pnV")) != -1) {
-    switch (opt) {
+  while ((opt = getopt_long(argc, argv, "pnV", long_options, 0)) != -1) {
       case 'p':
-        flag_p = true;
         printf("p\n");
         break;
       case 'n':
-        flag_n = true;
         printf("n\n");
         break;
 
       case 'V':
-        flag_V = true;
         printf("V\n");
         break;
 
