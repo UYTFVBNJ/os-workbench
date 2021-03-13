52066f7445038a8c67f5ffab1d2db4b5be055d0d
tracer-nju <tracer@nju.edu.cn>
2021-03-13 10:46:39 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: d25dd1313e4f4e03a021e3bd5cee4d5b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  10:46:39 up 14:03,  1 user,  load average: 1.06, 0.92, 0.80

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,22 +5,25 @@
 int flag_p = 0, flag_n = 0, flag_V = 0;
 
 int main(int argc, char* argv[]) {
-  static struct option long_options[] = {{"show-pids", no_argument, &flag_p, 'p'},
-                                         {"numeric-sort", no_argument, &flag_n, 'n'},
-                                         {"version", no_argument, &flag_V, 'V'},
+  static struct option long_options[] = {{"show-pids", no_argument, NULL, 'p'},
+                                         {"numeric-sort", no_argument, NULL, 'n'},
+                                         {"version", no_argument, NULL, 'V'},
                                          {0, 0, 0, 0}};
 
   int opt;
   while ((opt = getopt_long(argc, argv, "pnV", long_options, 0)) != -1) {
     switch (opt) {
       case 'p':
+        flag_p = 1;
         printf("p\n");
         break;
       case 'n':
+        flag_n = 1;
         printf("n\n");
         break;
 
       case 'V':
+        flag_V = 1;
         printf("V\n");
         break;
 
