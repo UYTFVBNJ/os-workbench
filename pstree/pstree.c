2e12368709c19932f521a50d003f9816e96e0985
tracer-nju <tracer@nju.edu.cn>
2021-03-13 12:46:03 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: d25dd1313e4f4e03a021e3bd5cee4d5b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  12:46:03 up 16:03,  1 user,  load average: 1.09, 1.06, 0.89

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,18 @@
 #include <assert.h>
+#include <dirent.h>
 #include <getopt.h>
 #include <stdio.h>
-#include <dirent.h>
 
 int flag_p = 0, flag_n = 0, flag_V = 0;
 
-int main(int argc, char* argv[]) {
+int is_num(char *str) {
+  for (int i = 0; str[i] != '\0'; i++)
+    if (str[i] < '0' || str[i] > '9') return 0;
+
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
   static struct option long_options[] = {
       {"show-pids", no_argument, NULL, 'p'},
       {"numeric-sort", no_argument, NULL, 'n'},
@@ -40,10 +47,11 @@ int main(int argc, char* argv[]) {
   assert(d);
 
   struct dirent *dir;
-  while ((dir = readdir(d)) != NULL) {
-    printf("%s\n", dir->d_name);
-  }
+  while ((dir = readdir(d)) != NULL)
+    if (dir->d_type == DT_DIR && is_num(dir->d_name)) {
+      printf("%s\n", dir->d_name);
+      // FILE fd = fopen("/proc/")
+    }
 
-  // FILE fd = fopen("/proc/")
   return 0;
 }
