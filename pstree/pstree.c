242bd0232b7f0215fb8a6c01c0ed0863be317b23
tracer-nju <tracer@nju.edu.cn>
2021-03-13 10:25:00 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: d25dd1313e4f4e03a021e3bd5cee4d5b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  10:25:00 up 13:42,  1 user,  load average: 0.18, 0.49, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,38 @@
-#include <stdio.h>
 #include <assert.h>
+#include <getopt.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+struct option long_options[] = {
+  {"show-pids",    no_argument, 0, 'p'},
+  {"numeric-sort", no_argument, 0, 'n'},
+  {"version",      no_argument, 0, 'V'},
+  {0,              0,           0,  0 }
+}
+
+int main(int argc, char* argv[]) {
+  bool flag_p = false, flag_n = false, flag_V = false;
+  int opt;
+  while ((opt = getopt_long(argc, argv, "pnV")) != -1) {
+    switch (opt) {
+      case 'p':
+        flag_p = true;
+        printf("p\n");
+        break;
+      case 'n':
+        flag_n = true;
+        printf("n\n");
+        break;
+
+      case 'V':
+        flag_V = true;
+        printf("V\n");
+        break;
+
+      default:
+        break;
+    }
   }
-  assert(!argv[argc]);
+
+  // FILE fd = fopen("/proc/")
   return 0;
 }
