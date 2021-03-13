8988a5cd6cc627a63c3957f724b545e57f059fd6
tracer-nju <tracer@nju.edu.cn>
2021-03-13 11:04:09 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: d25dd1313e4f4e03a021e3bd5cee4d5b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  11:04:10 up 14:21,  1 user,  load average: 0.85, 0.35, 0.45

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,12 +5,13 @@
 int flag_p = 0, flag_n = 0, flag_V = 0;
 
 int main(int argc, char* argv[]) {
-  static struct option long_options[] = {{"show-pids", no_argument, NULL, 'p'},
-                                         {"numeric-sort", no_argument, NULL, 'n'},
-                                         {"version", no_argument, NULL, 'V'},
-                                         {0, 0, 0, 0}};
+  static struct option long_options[] = {
+      {"show-pids", no_argument, NULL, 'p'},
+      {"numeric-sort", no_argument, NULL, 'n'},
+      {"version", no_argument, NULL, 'V'},
+      {0, 0, 0, 0}};
 
-  int opt;
+  int opt = 0;
   while ((opt = getopt_long(argc, argv, "pnV", long_options, 0)) != -1) {
     switch (opt) {
       case 'p':
@@ -32,7 +33,16 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  printf("%d %d %d\n", flag_p, flag_n, flag_V);
+  printf("flags: %d %d %d\n", flag_p, flag_n, flag_V);
+
+  DIR *d = opendir("/proc");
+  assert(d);
+
+  struct dirent *dir;
+  while ((dir = readdir(d)) != NULL) {
+    printf("%s\n", dir->d_name);
+  }
+
   // FILE fd = fopen("/proc/")
   return 0;
 }
