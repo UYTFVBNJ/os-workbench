e98852c99264ff018bc52016b69de8eeb9abe5a5
tracer-nju <tracer@nju.edu.cn>
2021-03-16 21:18:54 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 31fb2af34670414aaf121fc31b3ef2e3   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  21:18:54 up 52 min,  1 user,  load average: 0.79, 0.65, 0.57

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -55,10 +55,13 @@ void get_thread_info(char *filename, pid_t *main_pid) {
   char name_buf[256];
   sscanf(buf, "%d %s %c %d", &tmp, name_buf, &proc_state, &proc_ppid);
 
+  printf("%s\n", name_buf);
   name_buf[strlen(name_buf) - 1] = '\0';
 
+  printf("%s\n", name_buf);
   sscanf(name_buf + 1, "%s", procs[proc_pid].name);
 
+  printf("%s\n", procs[proc_pid].name);
   if (*main_pid == 0) {
     procs[proc_pid].ppid = proc_ppid;
     *main_pid = proc_pid;
