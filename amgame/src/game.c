1e14e521b5f46b7902dc25a486c0718e458f2178
tracer-nju <tracer@nju.edu.cn>
2021-03-18 21:49:21 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 04403c8c36994b5faf85df9e07c5d49b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  21:49:21 up 35 min,  1 user,  load average: 0.23, 0.20, 0.10

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -111,7 +111,7 @@ void game_loop() {
   // printf("qwq\n");
   // while (1);
   // printf("qwq\n");
-  /*
+  // /*
   Time next_frame = 0;
   while (1) {
     while (uptime() < next_frame)
@@ -124,7 +124,7 @@ void game_loop() {
     game_progress();  // 处理一帧游戏逻辑，更新物体的位置等
     next_frame += 1000 / FPS;  // 计算下一帧的时间
   }
-  */
+  // */
 }
 
 // Operating system is a C program!
