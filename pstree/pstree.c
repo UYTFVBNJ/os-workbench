ab4bad56dbf48e6d6785e18e682239f82a8352d4
tracer-nju <tracer@nju.edu.cn>
2021-03-24 11:46:46 +0800

    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 1a75ee1e98d948c3b9fb36945573dcf1   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  11:46:47 up 20 min,  1 user,  load average: 0.86, 0.62, 0.41

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -183,6 +183,7 @@ void input(int argc, char *argv[]) {
 
         fputs("pstree (PSmisc) UNKNOWN\n\
 Copyright (C) 1993-2019 Werner Almesberger and Craig Small\n\
+\n\
 PSmisc comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome to redistribute it under\n\
 the terms of the GNU General Public License.\n\
