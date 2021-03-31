5f35561bce404714529c69021327327f2543285d
tracer-nju <tracer@nju.edu.cn>
2021-03-31 17:14:08 +0800

    Static hostname: kubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 479a59b1d9f344f096019624ef24d98b            Boot ID: 2855d0e0c9a6404baf05b065fe73d6b5   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  17:14:08 up  3:13,  5 users,  load average: 0.46, 0.39, 0.43

--- libco/co.c
+++ libco/co.c
@@ -1,24 +1,130 @@
 #include "co.h"
-#include <stdlib.h>
+
 #include <setjmp.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-struct co {
-  char *name;
-  void (*func)(void *); // co_start 指定的入口地址和参数
-  void *arg;
-
-  enum co_status status;  // 协程的状态
-  struct co *    waiter;  // 是否有其他协程在等待当前协程
-  jmp_buf        context; // 寄存器现场 (setjmp.h)
-  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
-};
-
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+static inline void stack_switch_call(void *sp, void *entry,
+                                     uintptr_t arg) {  // TODO + ret addr
+  asm volatile(
+#if __x86_64__
+      "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+      :
+      : "b"((uintptr_t)sp), "d"(entry), "a"(arg)
+#else
+      "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+      :
+      : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
+#endif
+  );
 }
 
-void co_wait(struct co *co) {
+co *co_current;
+co *co_pool[CO_POOL_SIZE];
+
+// co_constructor
+co *co_start(const char *name, void (*func)(void *), void *arg) {
+  co *p = (co *)malloc(sizeof(co));
+  assert(p != NULL);
+
+  p->name = name;
+  p->func = func;
+  p->arg = arg;
+
+  p->status = CO_NEW;
+  p->waiter = co_current;
+
+  // p->context = ;
+
+  int i;
+  for (i = 0; i < CO_POOL_SIZE; i++)
+    if (co_pool[i] == NULL) {
+      co_pool[i] = p;
+      break;
+    }
+  assert(i < CO_POOL_SIZE);
+
+  return p;
 }
 
-void co_yield() {
+void co_wait(co *co) {
+  assert(co != co_pool[0]);
+  
+  co_current->status = CO_WAITING;
+
+  switch (co->status) {
+    case CO_NEW:
+      int ret = setjmp(co_current->context);
+      if (ret == 0) stack_switch_call(co->stack + STACK_SIZE, co_base, co);
+      co_destroyer(co);
+      break;
+
+    case CO_RUNNING:
+    case CO_WAITING:
+      co_yield();
+      break;
+
+    case CO_DEAD:
+      assert(0);
+      break;
+
+    default:
+      assert(0);
+  }
+
+  co_current->status = CO_RUNNING;
+}
+
+void co_yield() {  // can switch to itself
+  int ret = setjmp(co_current->context);
+  if (ret == 0) {
+    co *co = co_sheduler();
+    switch (co->status) {
+      case CO_NEW:
+        stack_switch_call(co->stack + STACK_SIZE, co_base, co);
+        break;
+
+      case CO_RUNNING:
+        longjmp(co->context, 1);
+        break;
+
+      default:
+        assert(0);
+    }
+  }
+}
+
+static co *co_sheduler() {
+  for (int i = 0; i < CO_POOL_SIZE; i++)
+    if (co_pool[i] != NULL &&
+        (co_pool[i]->status == CO_NEW || co_pool[i]->status == CO_RUNNING))
+      return co_pool[i];
+
+  assert(0);
+}
+
+static void co_destroyer(co *co) {
+  free(co);
+
+  int i;
+  for (i = 0; i < CO_POOL_SIZE; i++)
+    if (co_pool[i] == co) {
+      co_pool[i] = NULL;
+      break;
+    }
+  assert(i < CO_POOL_SIZE);
+}
+
+static void co_base(co *co) {
+  co->status = CO_RUNNING;
+  co->func(co->arg);
+  longjmp(co->waiter->context, 1);
+}
+
+static __attribute__((constructor)) void co_current_main() {
+  co_pool[0] = co_start("main", main, NULL);
+  co_pool[0]->status = CO_RUNNING;
+  printf("co_current: %p\n", co_current);
+  co_pool[0]->waiter = NULL;
+  co_current = co_pool[0];
 }
--- libco/co.h
+++ libco/co.h
@@ -2,7 +2,7 @@ struct co* co_start(const char *name, void (*func)(void *), void *arg);
 void co_yield();
 void co_wait(struct co *co);
 
-enum {
+enum co_status{
   CO_NEW = 1, // 新创建，还未执行过
   CO_RUNNING, // 已经执行过
   CO_WAITING, // 在 co_wait 上等待
@@ -10,3 +10,17 @@ enum {
 };
 
 #define STACK_SIZE 4096
+#define CO_POOL_SIZE 128
+
+struct co {
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+  enum co_status status;  // 协程的状态
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
+};
+
+typedef struct co co;
