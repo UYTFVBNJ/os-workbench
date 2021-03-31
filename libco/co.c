8079b5223aec17436c5dbec9b7352de639512224
tracer-nju <tracer@nju.edu.cn>
2021-03-31 14:38:23 +0800

    Static hostname: kubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 479a59b1d9f344f096019624ef24d98b            Boot ID: 2855d0e0c9a6404baf05b065fe73d6b5   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  14:38:23 up 37 min,  4 users,  load average: 1.04, 0.64, 0.41

--- libco/co.c
+++ libco/co.c
@@ -1,7 +1,16 @@
 #include "co.h"
 #include <stdlib.h>
+#include <setjmp.h>
 
 struct co {
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+  enum co_status status;  // 协程的状态
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
--- libco/co.h
+++ libco/co.h
@@ -1,3 +1,12 @@
 struct co* co_start(const char *name, void (*func)(void *), void *arg);
 void co_yield();
 void co_wait(struct co *co);
+
+enum {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
+};
+
+#define STACK_SIZE 4096
