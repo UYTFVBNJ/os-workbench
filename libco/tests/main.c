191fb819e3c7ce768151c38c96f15c8188233de2
tracer-nju <tracer@nju.edu.cn>
2021-04-02 13:09:53 +0800

    Static hostname: kubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 479a59b1d9f344f096019624ef24d98b            Boot ID: a9c87b6e385d426083467d2f05a7f0f4   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  13:09:53 up  1:32,  4 users,  load average: 1.11, 0.57, 0.36

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -151,16 +151,16 @@ static void test_3() {
 int main() {
     setbuf(stdout, NULL);
 
-    test_3();
-    
-    printf("\n\n");
-
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
     test_1();
 
     printf("\n\nTest #2. Expect: (libco-){200, 201, 202, ..., 399}\n");
     test_2();
 
+    printf("\n\n");
+
+    test_3();
+    
     printf("\n\n");
     
     return 0;
