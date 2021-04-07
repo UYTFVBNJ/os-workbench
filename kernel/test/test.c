b13cb75e1a09126bb41c46ebcd1aa346a3cdca7a
tracer-nju <tracer@nju.edu.cn>
2021-04-07 20:40:20 +0800

    Static hostname: kubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 479a59b1d9f344f096019624ef24d98b            Boot ID: 6339ffbfb94e42f6874e7773e07508ad   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-48-generic       Architecture: x86-64  20:40:20 up 31 min,  4 users,  load average: 0.55, 0.54, 0.59

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -1,9 +1,40 @@
-#include <kernel.h>
+#include <common.h>
 #include <klib.h>
+
+enum ops { OP_ALLOC = 1, OP_FREE };
+struct malloc_op {
+  enum ops type;
+  union { size_t sz; void *addr; };
+};
+
+void stress_test() {
+
+  while (1) {
+    struct malloc_op op = random_op();
+    switch (op.type) {
+      case OP_ALLOC: alloc_check(pmm->alloc(op.sz), op.sz); break;
+      case OP_FREE:  free(op.addr); break;
+    }
+  }
+}
 
 int main() {
   os->init();
-  mpe_init(os->run);
-  return 1;
+
+#ifdef TEST
+  printf("TESTING\n");
+#endif
+
+  mpe_init(stress_test);
 }
 
+/*
+static void entry(int tid) { pmm->alloc(128); }
+static void goodbye()      { printf("End.\n"); }
+int main() {
+  pmm->init();
+  for (int i = 0; i < 4; i++)
+    create(entry);
+  join(goodbye);
+}
+*/
