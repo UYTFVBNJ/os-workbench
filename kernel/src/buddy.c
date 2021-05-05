593b8a098374292279b532fab7e6239bf2fde607
tracer-nju <tracer@nju.edu.cn>
2021-05-05 13:38:18 +0800

    WSL2 hostname Zhan WSL2 machine-id 603af67e78c87e3f95526d1d6024a636  13:38:18 up 37 min,  0 users,  load average: 0.13, 0.09, 0.02

--- kernel/src/buddy.c
+++ kernel/src/buddy.c
@@ -9,7 +9,7 @@ void buddy_init(buddy_block_t *block, void *start, void *end) {
   block->TOTAL_SIZE =
       ((uintptr_t)end - (uintptr_t)start);  // TODO cal nearest 2^24
   assert(is_2_power(block->TOTAL_SIZE));
-  assert(block->TOTAL_SIZE >= 1 << 24);
+  // assert(block->TOTAL_SIZE >= 1 << 24);
   block->TOTAL_SHIFT = num2shift(block->TOTAL_SIZE);
 
   block->UNIT_SHIFT = 12;                       // 4 KiB
