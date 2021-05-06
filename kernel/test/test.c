4e07878c1abd52e9b1fd7a48f4d16559c299e69b
tracer-nju <tracer@nju.edu.cn>
2021-05-06 17:09:17 +0800

    WSL2 hostname Zhan WSL2 machine-id 603af67e78c87e3f95526d1d6024a636  17:09:17 up  7:08,  0 users,  load average: 0.00, 0.01, 0.00

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -1,62 +1,81 @@
 #include <common.h>
+#include <spinlock.h>
+
 #define ALLOC_SIZE (1 << 12)
 #define N ((1 << 12) - 32)
 
 enum ops { OP_NONE, OP_ALLOC, OP_FREE };
+
 struct malloc_op {
   enum ops type;
-  size_t sz;
+  size_t size;
   void *addr;
 };
 
-struct malloc_op *random_op() {
+spinlock_t op_lk;
+struct malloc_op op_arr[N];
+
+void op_insert(enum ops type, size_t size, void *addr) {
+  lock(&op_lk);
+
+  int i;
+  for (i = 0; i < N && op_arr[i].type != OP_NONE; i++)
+    ;
+
+  assert(i < N);
+
+  op_arr[i] = (struct malloc_op){.type = type, .size = size, .addr = addr};
+
+  unlock(&op_lk);
+}
+
+void random_op(struct malloc_op *op) {
   if (rand() % 2) {
-    int i;
-    for (i = 0; i < op_arr_cnt && op_arr[i].type != OP_NONE; i++)
-      ;
-    if (i == op_arr_cnt) op_arr_cnt++;
-    assert(op_arr_cnt <= N);
-    op_arr[i] = (struct malloc_op){.type = OP_ALLOC, .sz = ALLOC_SIZE};
-    return &op_arr[i];
+    // OP_ALLOC
+    *op = (struct malloc_op){.type = OP_ALLOC, .size = ALLOC_SIZE};
   } else {
+    // OP_FREE
+    lock(&op_lk);
+
     int i;
-    for (i = 0; i < op_arr_cnt && op_arr[i].type != OP_FREE; i++)
+    for (i = 0; i < N && op_arr[i].type != OP_FREE; i++)
       ;
-    if (i == op_arr_cnt)
-      return random_op();
-    else
-      return &op_arr[i];
+
+    if (i < N) {
+      *op = op_arr[i];
+      op_arr[i] = (struct malloc_op){.type = OP_NONE};
+    }
+
+    unlock(&op_lk);
+
+    if (i == N) random_op(op);
   }
 }
 
-void alloc_check(struct malloc_op *op) {
-  printf("acquiring %d bytes\n", op->sz);
-  void *addr = pmm->alloc(op->sz);
+void *alloc_check(struct malloc_op *op) {
+  printf("acquiring %d bytes\n", op->size);
+  void *addr = pmm->alloc(op->size);
   printf("got %p \n", addr);
-
-  op->type = OP_FREE;
-  op->addr = addr;
 }
 
 void free_check(struct malloc_op *op) {
-  printf("freeing mem of %d bytes at %p\n", op->sz, op->addr);
+  printf("freeing mem of %d bytes at %p\n", op->size, op->addr);
   pmm->free(op->addr);
-  op->type = OP_NONE;
-  printf("%d bytes freed at %p\n", op->sz, op->addr);
+  printf("%d bytes freed at %p\n", op->size, op->addr);
 }
 
 void stress_test() {
-  struct malloc_op op_arr[N];
-  int op_arr_cnt = 0;
-
   while (1) {
-    struct malloc_op *op = random_op();
-    switch (op->type) {
+    struct malloc_op op;
+    random_op(&op);
+
+    switch (op.type) {
       case OP_ALLOC:
-        alloc_check(op);
+        void *addr = alloc_check(&op);
+        op_insert(OP_FREE, op.size, addr);
         break;
       case OP_FREE:
-        free_check(op);
+        free_check(&op);
         break;
     }
   }
@@ -69,5 +88,9 @@ int main() {
   printf("TESTING\n");
 #endif
 
-  stress_test();
+  for (int i = 0; i < 4; i++) create(stress_test);
+
+  join(NULL);
+
+  // stress_test();
 }
