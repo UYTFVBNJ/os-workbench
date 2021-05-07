f7cea193291b96db98e08478ef6192cf13cd19fd
tracer-nju <tracer@nju.edu.cn>
2021-05-07 20:03:49 +0800

    WSL2 hostname Zhan WSL2 machine-id 603af67e78c87e3f95526d1d6024a636  20:03:49 up 15 min,  0 users,  load average: 0.13, 0.07, 0.06

--- kernel/test/pmm_test.c
+++ kernel/test/pmm_test.c
@@ -6,9 +6,8 @@
 // #define ALLOC_SIZE (1 << 5)
 #define N ((1 << (HEAP_XFT - 12)) - 100)
 // #define N 100
-#define SMP 4
 
-#define OUTPUT
+// #define OUTPUT
 
 enum ops { OP_NONE, OP_ALLOC, OP_FREE };
 
