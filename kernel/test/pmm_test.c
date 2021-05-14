6d0bf6a6c5c2877a3c0f8df499001f25dce79463
tracer-nju <tracer@nju.edu.cn>
2021-05-14 21:53:08 +0800

    WSL2 hostname kubuntu WSL2 machine-id 479a59b1d9f344f096019624ef24d98b  21:53:08 up 5 min,  3 users,  load average: 0.35, 0.38, 0.18

--- kernel/test/pmm_test.c
+++ kernel/test/pmm_test.c
@@ -3,10 +3,22 @@
 #include <spinlock.h>
 
 // #define ALLOC_SIZE (1 << 14)
-// #define ALLOC_SIZE (1 << 12)
-#define ALLOC_SIZE (1 << 5)
+#define ALLOC_SIZE (1 << 12)
+// #define ALLOC_SIZE (1 << 5)
 #define N ((1 << (HEAP_XFT - 12)) - 100)
 // #define N 100
+
+// struct workload
+// {
+//   int pr[16], sum; // sum = pr[0] + pr[1] + ... pr[N-1]
+//                    // roll(0, sum-1) => allocation size
+// };
+
+// static struct workload
+//   wl_typical = { .pr = { 10, 0, 0, 40, 50, 40, 30, 20, 10, 4, 2, 1 } },
+//   wl_stress = { .pr = { 1, 0, 0, 400, 200, 100, 1, 1, 1, 1, 1, 1 } },
+//   wl_page = { .pr = { 10, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+// static struct workload* workload = &wl_typical;
 
 #define RATE 2
 
@@ -21,18 +33,26 @@ int chk;
 spinlock_t free_cnt_lk;
 int free_cnt;
 
-enum ops { OP_NONE, OP_ALLOC, OP_FREE };
+enum ops
+{
+  OP_NONE,
+  OP_ALLOC,
+  OP_FREE
+};
 
-struct malloc_op {
+struct malloc_op
+{
   enum ops type;
   size_t size;
-  void *addr;
+  void* addr;
 };
 
 spinlock_t op_lk;
 struct malloc_op op_arr[N];
 
-static void op_insert(enum ops type, size_t size, void *addr) {
+static void
+op_insert(enum ops type, size_t size, void* addr)
+{
   lock(&op_lk);
 
   int i;
@@ -41,15 +61,17 @@ static void op_insert(enum ops type, size_t size, void *addr) {
 
   assert(i < N);
 
-  op_arr[i] = (struct malloc_op){.type = type, .size = size, .addr = addr};
+  op_arr[i] = (struct malloc_op){ .type = type, .size = size, .addr = addr };
 
   unlock(&op_lk);
 }
 
-static void random_op(struct malloc_op *op) {
+static void
+random_op(struct malloc_op* op)
+{
   if (rand() % RATE) {
     // OP_ALLOC
-    *op = (struct malloc_op){.type = OP_ALLOC, .size = ALLOC_SIZE};
+    *op = (struct malloc_op){ .type = OP_ALLOC, .size = ALLOC_SIZE };
   } else {
     // OP_FREE
     lock(&op_lk);
@@ -60,21 +82,24 @@ static void random_op(struct malloc_op *op) {
 
     if (i < N) {
       *op = op_arr[i];
-      op_arr[i] = (struct malloc_op){.type = OP_NONE};
+      op_arr[i] = (struct malloc_op){ .type = OP_NONE };
     }
 
     unlock(&op_lk);
 
-    if (i == N) random_op(op);
+    if (i == N)
+      random_op(op);
   }
 }
 
-void pmm_test_paint(int32_t *addr, size_t size, int key) {
+void
+pmm_test_paint(int32_t* addr, size_t size, int key)
+{
   size /= sizeof(int32_t);
   lock(&chk_lk);
   chk++;
   assert(chk == 1);
-  for (int32_t *chk_ptr = addr; chk_ptr < addr + size; chk_ptr++) {
+  for (int32_t* chk_ptr = addr; chk_ptr < addr + size; chk_ptr++) {
     if (*chk_ptr == USED(key))
       printf("double alloc at %p : %d\n", chk_ptr, *chk_ptr);
     assert(*chk_ptr != USED(key));
@@ -84,12 +109,14 @@ void pmm_test_paint(int32_t *addr, size_t size, int key) {
   unlock(&chk_lk);
 }
 
-void pmm_test_check(int32_t *addr, size_t size, int key) {
+void
+pmm_test_check(int32_t* addr, size_t size, int key)
+{
   size /= sizeof(int32_t);
   lock(&chk_lk);
   chk++;
   assert(chk == 1);
-  for (int32_t *chk_ptr = addr; chk_ptr < addr + size; chk_ptr++) {
+  for (int32_t* chk_ptr = addr; chk_ptr < addr + size; chk_ptr++) {
     if (*chk_ptr != USED(key))
       printf("double free at %p : %d\n", chk_ptr, *chk_ptr);
     assert(*chk_ptr == USED(key));
@@ -99,21 +126,28 @@ void pmm_test_check(int32_t *addr, size_t size, int key) {
   unlock(&chk_lk);
 }
 
-static void *alloc_check(struct malloc_op *op) {
+static void*
+alloc_check(struct malloc_op* op)
+{
 #ifdef OUTPUT
   printf("cpu %d acquiring %d bytes\n", cpu_current(), op->size);
 #endif
-  void *addr = pmm->alloc(op->size);
-  if (addr != NULL) pmm_test_paint(addr, op->size, op->size);
+  void* addr = pmm->alloc(op->size);
+  if (addr != NULL)
+    pmm_test_paint(addr, op->size, op->size);
 #ifdef OUTPUT
   printf("cpu %d got %p \n", cpu_current(), addr);
 #endif
   return addr;
 }
 
-static void free_check(struct malloc_op *op) {
+static void
+free_check(struct malloc_op* op)
+{
 #ifdef OUTPUT
-  printf("cpu %d freeing mem of %d bytes at %p\n", cpu_current(), op->size,
+  printf("cpu %d freeing mem of %d bytes at %p\n",
+         cpu_current(),
+         op->size,
          op->addr);
 #endif
   pmm_test_check(op->addr, op->size, op->size);
@@ -126,15 +160,18 @@ static void free_check(struct malloc_op *op) {
 #endif
 }
 
-static void stress_test() {
+static void
+stress_test()
+{
   while (1) {
     struct malloc_op op;
     random_op(&op);
 
     switch (op.type) {
       case OP_ALLOC:;
-        void *addr = alloc_check(&op);
-        if (addr != NULL) op_insert(OP_FREE, op.size, addr);
+        void* addr = alloc_check(&op);
+        if (addr != NULL)
+          op_insert(OP_FREE, op.size, addr);
         break;
       case OP_FREE:
         free_check(&op);
@@ -154,7 +191,9 @@ static void stress_test() {
   }
 }
 
-void pmm_test() {
+void
+pmm_test()
+{
 #ifdef TEST
   printf("PMM_TEST of cpuid %d\n", cpu_current());
 #endif
