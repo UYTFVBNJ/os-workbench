53dcb1529671ac96d5a233b80b2c8aa1fda6a28b
tracer-nju <tracer@nju.edu.cn>
2021-05-17 22:04:46 +0800

    WSL2 hostname kubuntu WSL2 machine-id 479a59b1d9f344f096019624ef24d98b  22:04:46 up  1:19,  3 users,  load average: 0.65, 0.54, 0.84

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -2,15 +2,21 @@
 #include <spinlock.h>
 #include <threads.h>
 
-void lock(spinlock_t *lk) {
+void
+lock(spinlock_t* lk)
+{
   //   iset(false); // TODO
   int cnt = 0;
   while (atomic_xchg(&lk->locked, 1)) {
+#ifdef TEST
     assert(cnt++ < 100000000);
+#endif
   };
 }
 
-void unlock(spinlock_t *lk) {
+void
+unlock(spinlock_t* lk)
+{
   atomic_xchg(&lk->locked, 0);
   //   iset(true); // TODO
 }
