b3107c19ddda8cf9eb9c7f1598acafbca85c56d5
tracer-nju <tracer@nju.edu.cn>
2021-05-20 00:42:07 +0800

    Static hostname: kubuntu          Icon name: computer-laptop            Chassis: laptop         Machine ID: 479a59b1d9f344f096019624ef24d98b            Boot ID: c3616476015b4fccaf44f0524215ea7b   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  00:42:07 up 10 min,  3 users,  load average: 0.53, 0.46, 0.26

--- kernel/src/minilib.c
+++ kernel/src/minilib.c
@@ -33,7 +33,7 @@ list_delete(list_t* list, node_t* node)
 bool
 is_2_power(int64_t n)
 {
-  for (int64_t i = 1; i <= INT64_MAX >> 1; i <<= 1)
+  for (int64_t i = 1; i <= INT64_MAX; i <<= 1)
     if (i == n)
       return true;
   return false;
